ea18712ba6337b808843d4f815b7c48f505affc3
bowen.yan <bowen.yan@siteview.com>
2019-04-03 16:34:04 +0800

    use actix version 0.7.11

--- src/web_server.rs
+++ src/web_server.rs
@@ -1,16 +1,22 @@
-use actix_files;
-use actix_web::{middleware, App, HttpServer};
+extern crate actix_web;
+
+use actix_web::fs::StaticFiles;
+use actix_web::{middleware, App, server::HttpServer};
 
 pub fn web_server(port: &str, data_dir: &str) -> std::io::Result<()> {
     let data_dir = data_dir.to_string();
-    HttpServer::new(move || {
-        let data_dir_clone = data_dir.clone();
+
+    HttpServer::new(move|| {
         App::new()
-            .wrap(middleware::Logger::default())
-            .service(
-                actix_files::Files::new("/", data_dir_clone).index_file("index.html"),
+            .middleware(middleware::Logger::default())
+            .handler("/",
+                     StaticFiles::new(
+                         &data_dir)
+                         .expect("Web server load static files failed.")
+                         .index_file("index.html"),
             )
     })
         .bind("0.0.0.0:".to_string() + port)?
-        .run()
+        .run();
+    Ok(())
 }
